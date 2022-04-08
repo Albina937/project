9f765cd98f8f364bcaf8532b7e58163f335d5c5c
Albina937 <82778165+Albina937@users.noreply.github.com>
2022-04-08 09:24:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+// "use strict"
+// let number=4;
+// console.log (4/0);
+// console.log(`string`*9);
+
+// const persone ="Alex";
+// const bool =true;
+// // console.log(someting);
+// let und;
+// console.log(und);
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// // console.log(obj.name);
+// console.log(obj ["name"]);
+
+// let arr =[`plum.png`,`orange.jpg`,`apple.bmp`, {},[]];
+// console.log(arr[1]);
+
+"use strict";
+
+// alert(`Hello`);
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt ("Вам есть 18", "18") ;
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt(`Как ваше имя?`, ``);
+// answers[1] = prompt(`Как ваша фамилия?`, ``);
+// answers[2] = prompt(`Сколько вам лет?`, ``);
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// "use srict";
+
+// const category = `toys`;
+
+// console.log(`https://www.youtube.com/watch?v=X1ElbhY4Kw4&ab_channel=%D0%A1%D0%A2%D0%A1/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+"use strict";
+
+console.log(`arr` + " - object");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2+ 2 * 2 !== `6`);
+
+const isCheked = true,
+      isClose = true;
+
+console.log (isCheked || !isClose);
+
+    
